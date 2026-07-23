da8d2aebac17373720c05280c8e9a05a597a9662
avyaypandey <pandeyavyaymamta@gmail.com>
2026-07-23 07:56:01 +0530

    added git ci and config

---
+++ include/minidb/common/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstddef>
+
+namespace minidb {
+
+    constexpr std::size_t PAGE_SIZE = 4096;
+
+}
--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "minidb/common/config.h"
+
 TEST(HelloDBMS, EngineBoots)
 {
     EXPECT_TRUE(true);
@@ -8,4 +10,9 @@ TEST(HelloDBMS, EngineBoots)
 TEST(HelloDBMS, BasicMath)
 {
     EXPECT_EQ(2 + 2, 4);
+}
+
+TEST(ConfigTest, PageSizeIsCorrect)
+{
+    EXPECT_EQ(minidb::PAGE_SIZE, 4096);
 }
